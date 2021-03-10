881aa796bb08d0c3d1bebce205c1dd74fede9841
lucky <lucky3028@users.noreply.github.com>
2021-03-10 19:03:12 +0900

    [Fix] lint

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App: FC = () => (
         href="https://reactjs.org"
         target="_blank"
         rel="noopener noreferrer"
-      >
+      
         Learn React
       </a>
     </header>
