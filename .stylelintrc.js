29275509282b9e59f84560dcb4f69b49576b9d14
lucky <lucky3028@users.noreply.github.com>
2021-03-10 21:20:13 +0900

    [Fix] stylelintをCSS in JSに対応させる

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,15 +1,11 @@
 module.exports = {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-recess-order',
-  ],
-  plugins: [
-    'stylelint-order',
-  ],
-  ignoreFiles: [
-    '**/node_modules/**',
-  ],
+  extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
+  plugins: ['stylelint-order'],
+  ignoreFiles: ['**/node_modules/**'],
   rules: {
     'string-quotes': 'single',
+    'no-empty-source': null,
+    'no-eol-whitespace': null,
+    'no-missing-end-of-source-newline': null,
   },
 };
