6e86099400b48b743746a3e99c5534c45a849a18
lucky <lucky3028@users.noreply.github.com>
2021-03-10 19:00:30 +0900

    [Fix] lint

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,6 +1,6 @@
 import { ReportHandler } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals')
       .then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
@@ -9,7 +9,8 @@ const reportWebVitals = (onPerfEntry?: ReportHandler) => {
         getFCP(onPerfEntry);
         getLCP(onPerfEntry);
         getTTFB(onPerfEntry);
-      });
+      })
+      .catch((err) => console.error(err)); // eslint-disable-line no-console
   }
 };
 
