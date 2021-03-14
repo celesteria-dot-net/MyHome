08c36b614bcda8605b0ca450724b835e353d1c08
lucky <lucky3028@users.noreply.github.com>
2021-03-14 14:48:29 +0900

    [Add] 常時表示するボタン群

---
+++ src/components/organisms/HomeIcons.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+
+import BackToPageTop from 'containers/atoms/BackToPageTopButton'
+import FlowIcons from 'components/molecules/FlowIcons'
+
+const HomeIcons: FC = () => (
+  <FlowIcons>
+    <BackToPageTop />
+  </FlowIcons>
+)
+
+export default HomeIcons
