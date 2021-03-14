777b4015daaeba0d1b436217f276f47649532073
lucky <lucky3028@users.noreply.github.com>
2021-03-14 23:17:18 +0900

    [Add] 各投稿の詳細表示

--- src/components/pages/BlogPost.tsx
+++ src/components/pages/BlogPost.tsx
@@ -1,15 +1,25 @@
+/* eslint react/no-danger: 0 */
 import React, { FC } from 'react';
+import { Container, Divider, Header } from 'semantic-ui-react';
 
-import PageTitle from 'components/atoms/PageTitle';
 import { Post } from 'domains/microCms';
-import { Container } from 'semantic-ui-react';
+import PageTitle from 'components/atoms/PageTitle';
+import { formatDateOnly } from 'utils/date';
 
 const BlogPost: FC<{ post: Post }> = ({ post }) => (
-  <Container>
+  <>
     <PageTitle title={post.title} />
-    {post.id}
-    {post.content}
-  </Container>
+
+    <Container>
+      <Divider hidden />
+      <Header as="h1" content={post.title} textAlign="center" />
+      <Divider />
+      <p>作成日：{formatDateOnly(post.publishedAt)}</p>
+      <p>最終更新日：{formatDateOnly(post.revisedAt)}</p>
+      <Divider hidden />
+      <span dangerouslySetInnerHTML={{ __html: post.content }} />
+    </Container>
+  </>
 );
 
 export default BlogPost;
