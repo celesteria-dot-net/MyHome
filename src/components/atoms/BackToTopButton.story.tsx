082f89081a783fbd7d1e92088209702a7d6e1859
lucky <lucky3028@users.noreply.github.com>
2021-03-14 14:05:22 +0900

    [Remove] components/BackToTopButton

--- src/components/atoms/BackToTopButton.story.tsx
+++
@@ -1,8 +0,0 @@
-/** @jsxImportSource @emotion/react */
-import React, { FC } from 'react';
-
-import BackToTop from './BackToTopButton';
-
-export default { title: 'Atoms/BackToTop' };
-
-export const initial: FC = () => <BackToTop />;
