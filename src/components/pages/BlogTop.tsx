14d2b1054a32505d55700a2f411679a372841b82
lucky <lucky3028@users.noreply.github.com>
2021-03-15 16:22:36 +0900

    [Remove] 不要なdivider

--- src/components/pages/BlogTop.tsx
+++ src/components/pages/BlogTop.tsx
@@ -15,7 +15,6 @@ const BlogTop: FC = () => (
       <Divider hidden />
       <Header as="h1" content="投稿一覧" textAlign="center" />
       <Divider />
-      <Divider hidden />
       <PostList />
     </Container>
 
